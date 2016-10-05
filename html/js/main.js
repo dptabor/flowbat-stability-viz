f1bbe3b9f1b8855c301c1739eab1cec48e62f5cb
adorsk <adorsk@fas.harvard.edu>
2016-10-05 13:42:16 -0400

    add datatables selector

--- html/js/main.js
+++ html/js/main.js
@@ -1,9 +1,10 @@
-/* global d3 */
+/* global d3, $ */
 
 let DATA_URL = 'data/example_data.json'
 
 function main () {
   let viz = new PourvaixViz()
+  window.viz = viz
   d3.json(DATA_URL, function onLoadData (data) {
     viz.setState({data})
   })
@@ -12,29 +13,33 @@ function main () {
 class PourvaixViz {
   constructor () {
     this.dimensions = this.genDimensions()
-    this.svgRoot = this.genSvgRoot({dimensions: this.dimensions})
+    this.appRoot = this.genAppRoot()
+    this.svgRoot = this.genSvgRoot({
+      dimensions: this.dimensions,
+      parent: this.appRoot.select('#left-panel')
+    })
     this.chartRoot = this.genChartRoot({svgRoot: this.svgRoot,
                                        dimensions: this.dimensions})
+    this.selectorRoot = this.genSelectorRoot({
+      parent: this.appRoot.select('#right-panel')
+    })
+    this.dataTable = this.genDataTable({parent: this.selectorRoot})
     this.scales = {}
-    this.state = {}
+    this.state = {
+      moleculeSelection: {},
+    }
   }
 
-  genSvgRoot ({dimensions}) {
-    let svgRoot = d3.select('body').append('svg')
-      .attr('width', dimensions.outer.width)
-      .attr('height', dimensions.outer.height)
-      .append('g')
-      .attr('transform',
-            `translate(${dimensions.margin.left}, ${dimensions.margin.top})`)
-    svgRoot.append('rect')
-      .attr('class', 'outer')
-      .attr('width', dimensions.inner.width)
-      .attr('height', dimensions.inner.height)
-    return svgRoot
+  genAppRoot () {
+    let appRoot = d3.select('body').append('div')
+      .attr('id', 'app-root')
+    appRoot.append('div').attr('id', 'left-panel')
+    appRoot.append('div').attr('id', 'right-panel')
+    return appRoot
   }
 
   genDimensions () {
-    let margin = {top: 20, right: 20, bottom: 20, left: 20}
+    let margin = {top: 0, right: 0, bottom: 0, left: 0}
     let padding = {top: 60, right: 60, bottom: 60, left: 60}
     let outer = {width: 960, height: 500}
     let inner = {
@@ -48,6 +53,20 @@ class PourvaixViz {
     return { margin, padding, outer, inner, chart }
   }
 
+  genSvgRoot ({dimensions, parent}) {
+    let svgRoot = parent.append('svg')
+      .attr('width', dimensions.outer.width)
+      .attr('height', dimensions.outer.height)
+      .append('g')
+      .attr('transform',
+            `translate(${dimensions.margin.left}, ${dimensions.margin.top})`)
+    svgRoot.append('rect')
+      .attr('class', 'outer')
+      .attr('width', dimensions.inner.width)
+      .attr('height', dimensions.inner.height)
+    return svgRoot
+  }
+
   genChartRoot ({svgRoot, dimensions}) {
     let chartRoot = svgRoot.append('g')
       .attr('transform',
@@ -59,15 +78,82 @@ class PourvaixViz {
     return chartRoot
   }
 
+  genSelectorRoot ({parent}) {
+    let selectorRoot = parent.append('div')
+    selectorRoot.append('h4').html('Molecules')
+    return selectorRoot
+  }
+
+  genDataTable ({parent}) {
+    let dataTableId = 'datatable'
+    parent.append('table').attr('id', dataTableId)
+    let table = $(`#${dataTableId}`)
+    let dataTable = table.DataTable({
+      buttons: [
+        {
+          text: 'clear selection',
+          action: () => dataTable.rows().deselect()
+        }
+      ],
+      columns: [
+        {title: 'Name', data: 'label'},
+        {title: 'Tags', data: 'tags'},
+      ],
+      dom: 'Bfrti',
+      paging: false,
+      select: {
+        style: 'multi',
+      }
+    })
+
+    let _genSelectionHandler = ({selectionValue}) => {
+      let selectionHandler = (e, dt, type, indexes) => {
+        let moleculeSet = dataTable.rows(indexes).data().toArray()
+        let selectionUpdates = {}
+        for (let molecule of moleculeSet) {
+          selectionUpdates[molecule.id] = selectionValue
+        }
+        this._updateMoleculeSelection(selectionUpdates)
+      }
+      return selectionHandler
+    }
+    dataTable.on('select', _genSelectionHandler({selectionValue: true}))
+    dataTable.on('deselect', _genSelectionHandler({selectionValue: false}))
+
+    return dataTable
+  }
+
+  _updateMoleculeSelection (selectionUpdates) {
+    let updatedMoleculeSelection = Object.assign(
+      {}, this.state.moleculeSelection, selectionUpdates)
+    let prunedMoleculeSelection = {}
+    for (let moleculeId of Object.keys(updatedMoleculeSelection)) {
+      if (updatedMoleculeSelection[moleculeId]) {
+        prunedMoleculeSelection[moleculeId] = true
+      }
+    }
+    this.setState({moleculeSelection: prunedMoleculeSelection})
+  }
+
   setState (newProps) {
     let prevState = this.state
     this.state = Object.assign({}, this.state, newProps)
-    this.onChangeState(prevState, this.state)
-    this.render()
+    if (this.shouldUpdate(prevState)) {
+      this.onChangeState(prevState, this.state)
+    }
+  }
+
+  shouldUpdate (prevState) {
+    return !this._statesAreEqual(prevState, this.state)
+  }
+
+  _statesAreEqual (state1, state2) {
+    return (JSON.stringify(state1) === JSON.stringify(state2))
   }
 
   onChangeState (prevState) {
     this.updateScales(this.state)
+    this.render()
   }
 
   updateScales (state) {
@@ -113,9 +199,12 @@ class PourvaixViz {
   }
 
   render () {
+    console.log('render', this.state)
     this.renderAxes()
     this.renderStabilityWindow()
-    this.renderMolecules()
+    let molecules = this.state.data.molecules
+    this.renderMoleculesInChart(molecules)
+    this.renderMoleculesInDataTable(molecules)
   }
 
   renderAxes () {
@@ -150,13 +239,13 @@ class PourvaixViz {
     return stabilityWindowG
   }
 
-  renderMolecules () {
-    for (let molecule of this.state.data.molecules) {
-      this.renderMolecule(molecule)
+  renderMoleculesInChart (molecules) {
+    for (let molecule of molecules) {
+      this.renderMoleculeInChart(molecule)
     }
   }
 
-  renderMolecule (molecule) {
+  renderMoleculeInChart (molecule) {
     let moleculeG = this.chartRoot.append('g')
       .classed(`molecule ${molecule.id}`, true)
     let commonKwargs = {
@@ -195,6 +284,21 @@ class PourvaixViz {
       .attr('r', radius)
     return pointsG
   }
+
+  renderMoleculesInDataTable (molecules) {
+    this.dataTable.clear()
+    this.dataTable.rows.add(molecules)
+    this.dataTable.rows().eq(0).each((index) => {
+      let row = this.dataTable.row(index)
+      let molecule = row.data()
+      let isSelected = this.state.moleculeSelection[molecule.id]
+      if (isSelected) { row.select() }
+    })
+    this.dataTable.draw(false)
+  }
+
 }
 
-main()
+$(document).ready(function () {
+  main()
+})
